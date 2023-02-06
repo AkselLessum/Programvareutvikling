e97b4f9404628fab7fe862691359e586b5eb9fa2
akselhl <akselhl@stud.ntnu.no>
2023-02-06 17:35:51 +0100

    #21 #20 Created a rudimentary nav-bar

--- Toolio/Toolio/settings.py
+++ Toolio/Toolio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_URL = 'static/'
