37926c87916e1b1acdb21eb2ae7ad107539f0215
unknown <akselhl@stud.ntnu.no>
2023-02-09 18:04:16 +0100

    #3 #31 #32 Setup basic frontend and backend for ads

---
+++ Toolio/main/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+class DateInput(forms.DateInput):
+    input_type = "date"
+
+class createAdForm(forms.Form):
+    title = forms.CharField(label="Tittel", max_length=100)
+    date = forms.DateField(widget=DateInput(), label="Velg dato produktet er ledig frem til:")
+    price = forms.IntegerField(label="Pris")
+    description = forms.CharField(label="Beskrivelse", widget=forms.Textarea, max_length=500)
+    #adImage = forms.FileField(label="Bilde av redskapet")
--- Toolio/main/views.py
+++ Toolio/main/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import ad
+from .forms import createAdForm
 # Create your views here.
 
 def home(response):
-    return render(response, "main/base.html", {})
+    context = {
+        'adList': ad.objects.all()
+    }
 
+    return render(response, "main/home.html", context)
+
+def createAd(response):
+    if response.method == "POST":
+        form = createAdForm(response.POST)
+
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            date = form.cleaned_data["date"]
+            price = form.cleaned_data["price"]
+            description = form.cleaned_data["description"]
+            # Need to add for image as well
+            newAd = ad(title=title, date=date, price=price, description=description)
+            newAd.save()
+        
+        return render(response, "main/home.html", {})
+
+    else:
+        form = createAdForm()
+    return render(response, "main/createAd.html", {"form":form})
