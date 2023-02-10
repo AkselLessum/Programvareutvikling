9603ae481331e0b311eb390f1059d5a43bd72e7a
Ksenia Mordovets <kseniam@stud.ntnu.no>
2023-02-10 15:48:45 +0100

    #3 #24 feat: :sparkles: Add functionality for adding image when creating ad

--- Toolio/main/forms.py
+++ Toolio/main/forms.py
@@ -8,4 +8,4 @@ class createAdForm(forms.Form):
     date = forms.DateField(widget=DateInput(), label="Velg dato produktet er ledig frem til:")
     price = forms.IntegerField(label="Pris")
     description = forms.CharField(label="Beskrivelse", widget=forms.Textarea, max_length=500)
-    #adImage = forms.FileField(label="Bilde av redskapet")
+    adImage = forms.FileField(label="Bilde av redskapet")
