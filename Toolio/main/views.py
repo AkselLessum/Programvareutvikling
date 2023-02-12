76a77335a7c101fa956a2e28e66878f253f4842c
unknown <akselhl@stud.ntnu.no>
2023-02-12 16:03:17 +0100

    # Created frontend selection for request ad, and backend variable for it being a request. Is shown on home page as request

--- Toolio/main/views.py
+++ Toolio/main/views.py
@@ -16,13 +16,18 @@ def createAd(response):
         form = createAdForm(response.POST, response.FILES)
 
         if form.is_valid():
+            type = form.cleaned_data["type"]
             title = form.cleaned_data["title"]
             date = form.cleaned_data["date"]
             price = form.cleaned_data["price"]
             description = form.cleaned_data["description"]
             adImage = form.cleaned_data["adImage"]
             # Need to add for image as well
-            newAd = ad(title=title, date=date, price=price, description=description, image=adImage)
+            if(type == "requestAd"):
+                request = True
+            else:
+                request = False
+            newAd = ad(isRequest=request, title=title, date=date, price=price, description=description, image=adImage)
             newAd.save()
         
         return render(response, "main/home.html", {})
