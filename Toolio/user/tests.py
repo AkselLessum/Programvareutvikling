3f7b2c0d457802bef4dd333c6c2421fb54b9b4ca
Iver Ringheim <iver.ringheim@gmail.com>
2023-02-13 15:50:29 +0100

    Added tests for login functionality

--- Toolio/user/tests.py
+++ Toolio/user/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class LogInTest(TestCase):
+    # sets up the conditions necessary to test the login
+    def setUp(self):
+        self.credentials = {
+            'username': 'test',
+            'password': 'user'}
+
+        # creates a User objects with the specified credentials
+        User.objects.create_user(**self.credentials)
+
+    def test_loginResponse(self):
+        # makes a post request to log in
+        response = self.client.post('/login/', self.credentials, follow=True)
+
+        # checks that status code of response is as intended (200)
+        self.assertEquals(response.status_code, 200)
+
+        # checks that the user is authenticated
+        self.assertTrue(response.context['user'].is_authenticated)
