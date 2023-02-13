838efe56d8c7c17381bd40a24b65b299c40c49ad
Iver Ringheim <iver.ringheim@ntnu.no>
2023-02-13 19:38:05 +0100

    2.1/2.3 Create login functionality and tests

--- Toolio/Toolio/urls.py
+++ Toolio/Toolio/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
+    path('', include("django.contrib.auth.urls")),
 ]
 
 if settings.DEBUG:
