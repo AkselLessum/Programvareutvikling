782b919613dee5626e3736e6c63fc2f8c7b5bb28
kseniam <kseniam@stud.ntnu.no>
2023-02-20 10:23:36 +0100

    style: :heavy_minus_sign: Remove unecessary imports

--- Toolio/user/admin.py
+++ Toolio/user/admin.py
@@ -1,6 +1,2 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
-from django import forms
+
