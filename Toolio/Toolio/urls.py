bf06b580b8f52689b7db04fac40e583d795b6651
kseniam <kseniam@stud.ntnu.no>
2023-02-20 01:33:00 +0100

    #49 fix: :bug: Fix login form
    - Fix login form, now compatible with the CustomUser model

--- Toolio/Toolio/urls.py
+++ Toolio/Toolio/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
-    path('', include("django.contrib.auth.urls")),
+    path('user/', include(('user.urls', 'user'), namespace='user')),
 ]
 
 if settings.DEBUG:
--- Toolio/user/forms.py
+++ Toolio/user/forms.py
@@ -1,20 +1,16 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 
 from .models import CustomUser
 
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
     phone_number = forms.IntegerField(required=True)
-    first_name = forms.CharField(required=True)
-    last_name = forms.CharField(required=True)
 
     class Meta:
         model = CustomUser
-        fields = ["first_name", "last_name", "username",
-                  "phone_number", "email", "password1", "password2"]
+        fields = ["first_name", "last_name", "username", "phone_number", "email", "password1", "password2"]
 
     def clean_phone_number(self):
         phone_number = self.cleaned_data.get('phone_number')
@@ -27,3 +23,7 @@ class RegisterForm(UserCreationForm):
         if CustomUser.objects.filter(email=email).exists():
             raise ValidationError("This email is already in use.")
         return email
+
+class LogInForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
