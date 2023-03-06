4cf8844b0cfee3d41dbd7752ace1bfac52b8c255
akselhl <akselhl@stud.ntnu.no>
2023-03-06 17:57:05 +0100

    Added default due to error handling

--- Toolio/main/models.py
+++ Toolio/main/models.py
@@ -9,7 +9,7 @@ class ad(models.Model):
     isRequest = models.BooleanField(default="False")
     isRented = models.BooleanField(default="False")
     title = models.CharField(max_length=100, default="")
-    category = models.CharField(max_length=100)
+    category = models.CharField(max_length=100, default="Annet")
     date = models.DateField(default=django.utils.timezone.now)
     price = models.IntegerField(default=0)
     description = models.CharField(max_length=500, default="")
