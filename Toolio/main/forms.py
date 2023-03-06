ceb0db26449878f6bfdbf2bccb10f76c484774eb
akselhl <akselhl@stud.ntnu.no>
2023-03-06 16:05:32 +0100

    #84 Added the category to the model and to all relevant forms. Co-Authored by: Iver Ringheim <iverri@stud.ntnu.no>

--- Toolio/main/forms.py
+++ Toolio/main/forms.py
@@ -7,6 +7,18 @@ AD_TYPES = [
     ("normalAd", "Annonse"),
     ("requestAd", "Ønskes leid")
 ]
+options = [
+    ("Annet", "Annet"),
+    ("Snekring og tømrerarbeid", "Snekring og tømrerarbeid"),
+    ("Maling og overflatebehandling", "Maling og overflatebehandling"),
+    ("Hagearbeid", "Hagearbeid"),
+    ("Rørleggerarbeid", "Rørleggerarbeid"),
+    ("Elektrikerarbeid", "Elektrikerarbeid"),
+    ("Bilreperasjon", "Bilrepereasjon"),
+    ("Møbelmontering", "Møbelmontering"),
+    ("Gulvlegging", "Gulvlegging"),
+    ("Rengjøring", "Rengjøring")
+]
 
 class DateInput(forms.DateInput):
     input_type = "date"
@@ -18,6 +30,7 @@ class createAdForm(forms.ModelForm):
     # Type's widget forms.RadioSelect has the attribute onchange, which gives the type-inputs this as a function call
     type = forms.ChoiceField(choices=AD_TYPES, widget=forms.RadioSelect(attrs={"onchange":"changeForm(this.id)"}))
     title = forms.CharField(label="Tittel", max_length=100, widget=forms.TextInput(attrs={"placeholder": "Toolio"}), label_suffix='')
+    category = forms.ChoiceField(label = "Kategori", choices = options)
     date = forms.DateField(widget=DateInput(), label="Produktet er ledig frem til")
     price = forms.IntegerField(label="Pris", widget=widgets.NumberInput(attrs={"style":"border-radius: 0 !important;"}))
     description = forms.CharField(label="Beskrivelse", widget=forms.Textarea(attrs={"placeholder": "Beskriv verktøyet", "rows": 3}), max_length=500, label_suffix='')
@@ -25,11 +38,12 @@ class createAdForm(forms.ModelForm):
 
     class Meta:
         model = ad
-        fields = ['type', 'title', 'date', 'price', 'description', 'image']
+        fields = ['type', 'title', 'category', 'date', 'price', 'description', 'image']
 
 
 class editAdForm(forms.ModelForm):
     title = forms.CharField(label="Tittel", max_length=100, label_suffix='')
+    category = forms.ChoiceField(label = "Katerogi", choices = options)
     date = forms.DateField(widget=DateInput(), label="Produktet er ledig frem til",label_suffix='')
     price = forms.IntegerField(label="Pris", widget=widgets.NumberInput(attrs={"style":"border-radius: 0 !important;"}), label_suffix='')
     description = forms.CharField(label="Beskrivelse",widget=forms.Textarea(attrs={'rows': 5, 'cols': 40}), max_length=500, label_suffix='')
@@ -38,16 +52,17 @@ class editAdForm(forms.ModelForm):
 
     class Meta:
         model = ad
-        fields = ('title', 'date', 'price','description',  'image', 'isRented')
+        fields = ('title', 'category', 'date', 'price','description',  'image', 'isRented')
 
 
 
 class editAdFormWanted(forms.ModelForm): # Edit "ønsket leid", no image in fields
     title = forms.CharField(label="Tittel", max_length=100, label_suffix='')
+    category = forms.ChoiceField(label = "Katerogi", choices = options)
     date = forms.DateField(widget=DateInput(), label="Ønsker å leie til",label_suffix='')
     price = forms.IntegerField(label="Budsjett", widget=widgets.NumberInput(attrs={"style":"border-radius: 0 !important;"}), label_suffix='')
     description = forms.CharField(label="Beskrivelse",widget=forms.Textarea(attrs={'rows': 5, 'cols': 40}), max_length=500, label_suffix='')
 
     class Meta:
         model = ad
-        fields = ('title', 'date', 'price','description')
+        fields = ('title', 'category', 'date', 'price','description')
