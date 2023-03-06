470b683b3102e38939161650a59c9c8ea0a0cf92
Andreas Granhøy-Lieng <andrgran@stud.ntnu.no>
2023-03-06 15:05:40 +0100

    midlertididig tull

--- Toolio/user/find_distance.py
+++ Toolio/user/find_distance.py
@@ -2,20 +2,13 @@ from geopy import Nominatim, distance
 from main.models import ad
 
 def get_ad_distance_dict(user_postal_code):
-        geolocator = Nominatim(user_agent="Toolio")
-        location_object_user = geolocator.geocode(user_postal_code, country_codes="NOR")
-        location_user = (location_object_user.latitude,
-                location_object_user.longitude)
-        
-        ad_distance_dict = {}
-        for ad_instance in ad.objects.all():
-            location_object_ad_user = geolocator.geocode(
-                ad_instance.user.postal_code, country_codes="NOR")
+    geolocator = Nominatim(user_agent="Toolio")
+    location_object_user = geolocator.geocode(user_postal_code, country_codes="NOR")
+    location_user = (location_object_user.latitude, location_object_user.longitude)
 
-            location_ad_user = (location_object_ad_user.latitude,
-                            location_object_ad_user.longitude)
+    ad_postal_codes = ad.objects.all().values_list('user__postal_code', flat=True)
 
-            distance_in_km = int(round(distance.distance(
-               location_user, location_ad_user).km))
-            ad_distance_dict[ad_instance.pk] = distance_in_km
-        return ad_distance_dict
+    ad_distance_dict = {ad_instance.pk: int(round(distance.distance(location_user, geolocator.geocode(postal_code, country_codes="NOR").point).km)) 
+                        for ad_instance, postal_code in zip(ad.objects.all(), ad_postal_codes)}
+
+    return ad_distance_dict
