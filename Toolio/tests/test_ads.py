1b0719de5773d5d2fb63146f005d5b1555b39780
Magnus Stavland Jakobsen <magnsjak@stud.ntnu.no>
2023-03-17 13:26:57 +0100

    Update unit tests to test negative negative price values. Add minimum value to forms to prevent negative price values when editing and creating. Co-authored-by: Ksenia Mordovets <kseniam@stud.ntnu.no>

--- Toolio/tests/test_ads.py
+++ Toolio/tests/test_ads.py
@@ -4,6 +4,7 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.contrib.auth import get_user_model
 from main.models import ad
 from user.models import CustomUser
+from main.forms import createAdForm
 
 
 class AdViewsTestCase(TestCase):
@@ -64,34 +65,104 @@ class AdViewsTestCase(TestCase):
             "image": SimpleUploadedFile("test_image.png", b"file_content", content_type="image/png")
         }
 
-        self.ad = ad.objects.create(**self.ad_data)
-        self.ad_request = ad.objects.create(**self.ad_data_request)
+        #ad.objects.create(**self.ad_data)
+        #ad.objects.create(**self.ad_data_request)
 
     def test_create_ad(self):
-        self.client.login(username="brukernavn", password="Bananmann1")
-        response = self.client.post(
-            self.ad_url, data=self.ad_data, follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue(ad.objects.filter(title="Test Ad").exists(), msg="FAILED: ad was not created")
+        #self.client.login(username="brukernavn", password="Bananmann1")
+        form_data = {
+        'title': 'Test ad',
+        'category': 'Annet',
+        'date': '2022-01-01',
+        'price': 100,
+        'description': 'This is a test ad',
+        'image': SimpleUploadedFile("test_image.jpg", b"file_content", content_type="image/jpeg"),
+        'type': 'normalAd'
+        }
+
+        form = createAdForm(data=form_data, files=form_data)
+        self.assertTrue(form.is_valid())
+        NewAd = form.save()
+        self.assertEqual(ad.objects.count(), 1)
+        self.assertTrue(ad.objects.filter(title="Test ad").exists(), msg="FAILED: ad was not created")
 
     
     def test_create_request_ad(self):
-        self.client.login(username="brukernavn", password="Bananmann1")
-        response = self.client.post(
-            self.ad_url, data=self.ad_data_request, follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue(ad.objects.filter(title="Test Ad").exists(), msg="FAILED: ad was not created")    
+        form_data = {
+        'title': 'Request ad',
+        'category': 'Annet',
+        'date': '2022-01-01',
+        'price': 100,
+        'description': 'This is a test ad',
+        'image': SimpleUploadedFile("test_image.jpg", b"file_content", content_type="image/jpeg"),
+        'type': 'requestAd'
+        }
+        form = createAdForm(data=form_data, files=form_data)
+        self.assertTrue(form.is_valid())
+        NewAd = form.save()
+        self.assertEqual(ad.objects.count(), 1)
+        self.assertTrue(ad.objects.filter(title="Request ad").exists(), msg="FAILED: ad was not created")    
 
     def test_create_no_title_ad(self):
-        self.client.login(username="brukernavn", password="Bananmann1")
-        response = self.client.post(
-            self.ad_url, data=self.no_title_ad_data, follow=True)
-        self.assertEqual(response.status_code, 200)
-        self.assertFalse(ad.objects.filter(title="").exists(), msg="FAILED: able to create ad without title")
+        form_data = {
+        'title': '',
+        'category': 'Annet',
+        'date': '2022-01-01',
+        'price': 100,
+        'description': 'This is a test ad with no title',
+        'image': SimpleUploadedFile("test_image.jpg", b"file_content", content_type="image/jpeg"),
+        'type': 'normalAd'
+        }
+        form = createAdForm(data=form_data, files=form_data)
+        print("Asserting that ad without title is not created...")
+        self.assertFalse(form.is_valid(), msg="FAILED: ad without title should not be valid")
+        try:
+            NewAd = form.save()
+        except:
+            self.assertEqual(ad.objects.count(), 0)
+            self.assertFalse(ad.objects.filter(title="").exists(), msg="FAILED: able to create ad without title")
+
+    def test_create_negative_price_ad(self):
+        form_data = {
+        'title': 'Negative Price Ad',
+        'category': 'Annet',
+        'date': '2022-01-01',
+        'price': -100,
+        'description': 'This is a test ad with negative price',
+        'image': SimpleUploadedFile("test_image.jpg", b"file_content", content_type="image/jpeg"),
+        'type': 'normalAd'
+        }
+        form = createAdForm(data=form_data, files=form_data)
+        print("Asserting that ad with negative price is not created...")
+        self.assertFalse(form.is_valid(), msg="FAILED: ad with negative price should not be valid")
+        try:
+            NewAd = form.save()
+        except:
+            self.assertEqual(ad.objects.count(), 0)
+            self.assertFalse(ad.objects.filter(title="Negative Price Ad").exists(), msg="FAILED: able to create ad with negative price")
+    
+    def test_create_negative_price_request(self):
+        form_data = {
+        'title': 'Negative Price Request',
+        'category': 'Annet',
+        'date': '2022-01-01',
+        'price': -100,
+        'description': 'This is a test request with negative price',
+        'image': SimpleUploadedFile("test_image.jpg", b"file_content", content_type="image/jpeg"),
+        'type': 'requestAd'
+        }
+        form = createAdForm(data=form_data, files=form_data)
+        print("Asserting that request with negative price is not created...")
+        self.assertFalse(form.is_valid(), msg="FAILED: request with negative price should not be valid")
+        try:
+            NewAd = form.save()
+        except:
+            self.assertEqual(ad.objects.count(), 0)
+            self.assertFalse(ad.objects.filter(title="Negative Price Request").exists(), msg="FAILED: able to create request with negative price")        
 
     def test_edit_ad(self): #tests if an "annonse" can be edited
         # Creates a new ad (annonse) object
-
+        ad.objects.create(**self.ad_data)
         self.client.login(username="brukernavn", password="Bananmann1")
         response = self.client.post(reverse('createAd'), data=self.ad_data, follow=True)
         self.assertEqual(response.status_code, 200)
@@ -118,29 +189,22 @@ class AdViewsTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         # Get the updated ad object and check if the values are updated correctly
         ad_obj = ad.objects.get(id=ad_id)
-        print()
         print("Asserting title change... Title should now be Updated Title:   ", ad_obj.title)
         self.assertEqual(ad_obj.title, "Updated Title", msg="FAILED TEST: ad title was not updated")
-        print()
         print("Asserting category change... Category should now be Hagearbeid:   ", ad_obj.category)
         self.assertEqual(ad_obj.category, "Hagearbeid", msg="FAILED TEST: ad category was not updated")
-        print()
         print("Asserting date change... Date should now be = 2023-03-16:   ", ad_obj.date)
         self.assertEqual(str(ad_obj.date), "2023-03-16", msg="FAILED TEST: ad date was not updated")
-        print()
         print("Asserting price change...   Price should now be 200:   ", ad_obj.price)
         self.assertEqual(ad_obj.price, 200, msg="FAILED TEST: ad price was not updated")
-        print()
         print("Asserting description change...   Description should now be Updated Description:   ",ad_obj.description)
         self.assertEqual(ad_obj.description, "Updated Description", msg="FAILED TEST: ad description was not updated")
-        print()
         print("Asserting isRented boolean value...   isRented should now be True:   ", ad_obj.isRented)
         self.assertEqual(ad_obj.isRented, True, msg="FAILED TEST: isRented boolean value was not updated")
-        print()
     
     def test_edit_request_ad(self): #tests if an "ønskes leid" can be edited, as this uses a different form
         # Creates a new request ad object
-
+        ad.objects.create(**self.ad_data_request)
         self.client.login(username="brukernavn", password="Bananmann1")
         response = self.client.post(reverse('createAd'), data=self.ad_data, follow=True)
         self.assertEqual(response.status_code, 200)
@@ -167,22 +231,16 @@ class AdViewsTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         # Get the updated ad object and check if the values are updated correctly
         ad_obj = ad.objects.get(id=ad_id)
-        print()
         print("Asserting request title change... Title should now be Updated Request Title:   ", ad_obj.title)
         self.assertEqual(ad_obj.title, "Updated Request Title", msg="FAILED TEST: ad title was not updated")
-        print()
         print("Asserting request category change... Category should now be Hagearbeid:   ", ad_obj.category)
         self.assertEqual(ad_obj.category, "Hagearbeid", msg="FAILED TEST: ad category was not updated")
-        print()
         print("Asserting request date change... Date should now be = 2023-03-16:   ", ad_obj.date)
         self.assertEqual(str(ad_obj.date), "2023-03-16", msg="FAILED TEST: ad date was not updated")
-        print()
         print("Asserting request price change...   Price should now be 200:   ", ad_obj.price)
         self.assertEqual(ad_obj.price, 200, msg="FAILED TEST: ad price was not updated")
-        print()
         print("Asserting request description change...   Description should now be Updated Request Description:   ",ad_obj.description)
         self.assertEqual(ad_obj.description, "Updated Request Description", msg="FAILED TEST: ad description was not updated")
-        print()
 
     #Tests if an ad is succesfully deleted from the database
     def test_delete_ad(self): 
@@ -190,8 +248,7 @@ class AdViewsTestCase(TestCase):
         self.client.login(username="brukernavn", password="Bananmann1")
     
         # Creates a new ad object
-        response = self.client.post(self.ad_url, data=self.ad_data, follow=True)
-        self.assertEqual(response.status_code, 200)
+        ad.objects.create(**self.ad_data)
         self.assertTrue(ad.objects.filter(title="Test Ad").exists(), msg="FAILED: ad was not created")
     
         # Gets the ad object and its id
@@ -204,11 +261,8 @@ class AdViewsTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
     
         # Check that the ad object no longer exists in the database
-        print()
         print("Asserting ad deletion...   ")
         self.assertFalse(ad.objects.filter(title="Test Ad").exists(), msg="FAILED: ad was not deleted")
-        print("Ad deletion asserted")
-        print()
 
 
 
--- Toolio/tests/test_user.py
+++ Toolio/tests/test_user.py
@@ -20,6 +20,17 @@ class BaseTest(TestCase):
             'password1': 'Sikkert123',
             'password2': 'Sikkert123',
         }
+        
+        self.user_invalid_username = {
+            'first_name': 'Navn',
+            'last_name': 'Navnesen',
+            'username': '?Navnesen?',
+            "phone_number": '12345678',
+            'postal_code': '4022',
+            'email': 'navn.navnesen@epostkasse.no',
+            'password1': 'Sikkert123',
+            'password2': 'Sikkert123',
+        }
 
         # A user profile with a duplicate username
         self.user_duplicate_username = {
@@ -126,12 +137,14 @@ class RegisterTest(BaseTest):
     def test_register_page_is_visible(self):
         response = self.client.get(self.register_url)
         self.assertEqual(response.status_code, 200)
+        print("Asserting that user page is visible...")
         self.assertTemplateUsed("user/register.html")
 
     # Checks if the user is able to register.
     def test_register_new_user(self):
         response = self.client.post(
-            self.register_url, self.user, format='text/html')
+        self.register_url, self.user, format='text/html')
+        print("Asserting that user is able to register...")
         self.assertEqual(response.status_code, 302)
 
     # Checks if the user is created and exists in the database.
@@ -141,24 +154,27 @@ class RegisterTest(BaseTest):
 
         user_exists = CustomUser.objects.filter(
             username=self.user["username"]).exists()
+        print("Asserting that user is succesfully created and exists in database...")
         self.assertTrue(user_exists, f"User was not created in the database.")
 
-    # Checks if the user with an invalid username is not created in the database.
-    def test_register_none_existent_in_database(self):
+
+    def test_register_invalid_username(self):
         self.client.post(
-            self.register_url, self.user, format='text/html')
+            self.register_url, self.user_invalid_username, format='text/html')
 
         user_exists = CustomUser.objects.filter(
-            username="NotAUserName").exists()
+            username="?Navnesen?").exists()
+        print("Asserting that user is unable to register with invalid characters in username...")
         self.assertFalse(user_exists, f"User was not created in the database.")
 
     # Checks if the user is not created when a duplicate username is used.
-    def test_duplucate_username(self):
+    def test_duplicate_username(self):
         response = self.client.post(
             self.register_url, self.user, format='text/html')
 
         response_dup = self.client.post(
             self.register_url, self.user_duplicate_username, format='text/html')
+        print("Asserting that user is unable to register with a duplicate username...")
         self.assertEqual(response_dup.status_code, 400)
 
     # Checks if the user is not able to register with a short password.
@@ -171,12 +187,14 @@ class RegisterTest(BaseTest):
     def test_cant_register_user_with_unmatching_passwords(self):
         response = self.client.post(
             self.register_url, self.user_unmatching_password, format='text/html')
+        print("Asserting that user is unable to register with unmatching passwords...")
         self.assertEqual(response.status_code, 400)
 
     # Checks if the user is not able to register with an invalid email.
     def test_cant_register_user_with_invalid_email(self):
         response = self.client.post(
             self.register_url, self.user_invalid_email, format='text/html')
+        print("Asserting that user is unable to register with invalid email...")
         self.assertEqual(response.status_code, 400)
 
     # Checks if the user is not able to register an invalid number.
@@ -184,6 +202,7 @@ class RegisterTest(BaseTest):
         self.client.post(self.register_url, self.user, format='text/html')
         response = self.client.post(
             self.register_url, self.user_invalid_number, format='text/html')
+        print("Asserting that user is unable to register with invalid phone number...")
         self.assertEqual(response.status_code, 400, msg="TEST FAILED: The user was able to register an invalid phone number")
     
     #Checks if the user is unable to register an invalid postal code.
@@ -191,6 +210,7 @@ class RegisterTest(BaseTest):
         self.client.post(self.register_url, self.user, format='text/html')
         response = self.client.post(
             self.register_url, self.user_invalid_postal_code, format='text/html')
+        print("Asserting that user is unable to register with invalid postal code...")
         self.assertEqual(response.status_code, 400, msg="TEST FAILED: The user was able to register an invalid postal code of 40222")
 
     
