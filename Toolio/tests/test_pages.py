b74d94b0c5b9241108646108a00ce0f47bb32416
Magnus Stavland Jakobsen <magnsjak@stud.ntnu.no>
2023-03-20 13:29:20 +0100

    Print statement for homepage

--- Toolio/tests/test_pages.py
+++ Toolio/tests/test_pages.py
@@ -10,6 +10,6 @@ class MainUrlsTest(TestCase):
         url = reverse("home")
         response = self.client.get(url)
         status_code = response.status_code
-        print("Asserts that the homepage looads...")
+        print("Asserting that the homepage loads...")
         self.assertTrue(
             status_code == 200, msg=f"TEST FAILED: The URL {url} returned a {response.status_code} status code.")
